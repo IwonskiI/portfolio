ac794881e14838c3751f4f4008aa8ce382ebca9b
Wonski <ricky2101@naver.com>
2024-01-23 22:25:41 +0900

    fix ESLint Error - ' escape

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
       <div className="bg-gray-100 dark:bg-slate-600/20">
         <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <span className="ml-3 text-xl">Wonski's PortFolio</span>
+            <span className="ml-3 text-xl">Wonski&lsquo;s PortFolio</span>
           </a>
           <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-            © 2024 한원석 -
+            © 2024 Wonseok Han -
             <a
               href="https://github.com/IwonskiI/"
               rel="noopener noreferrer"
